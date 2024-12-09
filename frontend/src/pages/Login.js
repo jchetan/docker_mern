56a7efddea39485416776b31c15f51aa7b71d440
Chetan Janardhana <janachet@amazon.com>
2024-12-09 15:02:25 +0000

    Add heading for Login and Register forms

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,36 +1,39 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'; 
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 import axios from 'axios'
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [status, setStatus] = useState('');
-    
+
     axios.defaults.withCredentials = true;
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
             const response = await axios.post(
                 "http://localhost:5000/login",
-                {email, password}
+                { email, password }
             )
             setStatus(response.data.status)
             console.log(response)
         } catch (error) {
             console.log(error)
-        }        
+        }
     }
 
     return (
         <div className="row justify-content-center my-2">
             <div className="col-4 border">
+                <div className="text-center">
+                    <h2>Login</h2>
+                </div>
                 <form onSubmit={handleSubmit}>
                     <div className="mb-3">
                         <label htmlFor="email" className="form-label">Email</label>
-                        <input 
-                            type="text" 
-                            className="form-control" 
+                        <input
+                            type="text"
+                            className="form-control"
                             id="email"
                             onChange={(e) => setEmail(e.target.value)}
                             value={email}
@@ -38,21 +41,21 @@ const Login = () => {
                     </div>
                     <div className="mb-3">
                         <label htmlFor="password" className="form-label">Password</label>
-                        <input 
-                            type="password" 
-                            className="form-control" 
-                            id="password" 
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="password"
                             onChange={(e) => setPassword(e.target.value)}
                             value={password}
                         />
-                    </div>                    
+                    </div>
                     <button type="submit" className="btn btn-primary mb-3">Login</button>
                     {
                         status &&
                         <div className="">
                             {status}
                         </div>
-                        
+
                     }
                     <h6>Not Registered?</h6>
                     <Link to="/register" className="btn btn-primary mb-3">Register</Link>
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,35 +1,38 @@
-import React, {useState} from 'react'
-import { Link } from 'react-router-dom'; 
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 import axios from 'axios'
 
 const Register = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [status, setStatus] = useState('');
-    
+
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
             const response = await axios.post(
                 "http://localhost:5000/register",
-                {email, password}
+                { email, password }
             )
             setStatus(response.data.status)
             console.log(response)
         } catch (error) {
             console.log(error)
-        }        
+        }
     }
 
     return (
         <div className="row justify-content-center my-2">
             <div className="col-4 border">
+                <div className="text-center">
+                    <h2>Register</h2>
+                </div>
                 <form onSubmit={handleSubmit}>
                     <div className="mb-3">
                         <label htmlFor="email" className="form-label">Email</label>
-                        <input 
-                            type="text" 
-                            className="form-control" 
+                        <input
+                            type="text"
+                            className="form-control"
                             id="email"
                             onChange={(e) => setEmail(e.target.value)}
                             value={email}
@@ -37,21 +40,21 @@ const Register = () => {
                     </div>
                     <div className="mb-3">
                         <label htmlFor="password" className="form-label">Password</label>
-                        <input 
-                            type="password" 
-                            className="form-control" 
-                            id="password" 
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="password"
                             onChange={(e) => setPassword(e.target.value)}
                             value={password}
                         />
-                    </div>                    
+                    </div>
                     <button type="submit" className="btn btn-primary mb-3">Register</button>
                     {
                         status &&
                         <div className="">
                             {status}
                         </div>
-                        
+
                     }
                     <h6>Already Registered?</h6>
                     <Link to="/login" className="btn btn-primary mb-3">Login</Link>
