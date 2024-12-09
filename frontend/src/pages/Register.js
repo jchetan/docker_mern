80cc02e28e2eea8463d6e13e56587db9bae26f56
Chetan Janardhana <janachet@amazon.com>
2024-12-09 15:26:47 +0000

    After Register show success screen and Login button

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 const Register = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [status, setStatus] = useState('');
+    const [registered, setRegistered] = useState(false);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -14,7 +14,9 @@ const Register = () => {
                 "http://localhost:5000/register",
                 { email, password }
             )
-            setStatus(response.data.status)
+            if (response.data.status === "success") {
+                setRegistered(true);
+            }            
             console.log(response)
         } catch (error) {
             console.log(error)
@@ -23,49 +25,58 @@ const Register = () => {
 
     return (
         <div className="row justify-content-center my-2">
-            <div className="col-4 border">
-                <div className="text-center">
-                    <h2>Register</h2>
-                </div>
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-3">
-                        <label htmlFor="email" className="form-label">Email</label>
-                        <input
-                            type="text"
-                            className="form-control"
-                            id="email"
-                            onChange={(e) => setEmail(e.target.value)}
-                            value={email}
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="password" className="form-label">Password</label>
-                        <input
-                            type="password"
-                            className="form-control"
-                            id="password"
-                            onChange={(e) => setPassword(e.target.value)}
-                            value={password}
-                        />
-                    </div>
+
+            {registered &&
+                <div className="col-4 border">
                     <div className="text-center">
-                        <button type="submit" className="btn btn-primary mb-3 w-50">Register</button>
+                        <h6>You have successfully Registered</h6>
+                        <h6>Login using the below link</h6>
+                        <Link to="/login" className="btn btn-primary mb-3 w-50">Login</Link>
+                    </div>
+                </div>
+            }
+
+            {!registered &&
+                <div className="col-4 border">
+                    <div className="text-center">
+                        <h2>Register</h2>
+                    </div>
+                    <form onSubmit={handleSubmit}>
+                        <div className="mb-3">
+                            <label htmlFor="email" className="form-label">Email</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="email"
+                                onChange={(e) => setEmail(e.target.value)}
+                                value={email}
+                            />
+                            <div className="">Email Error message</div>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="password" className="form-label">Password</label>
+                            <input
+                                type="password"
+                                className="form-control"
+                                id="password"
+                                onChange={(e) => setPassword(e.target.value)}
+                                value={password}
+                            />
+                            <div className="">Password Error message</div>
+                        </div>
+                        <div className="text-center">
+                            <button type="submit" className="btn btn-primary mb-3 w-50">Register</button>
+                        </div>                        
+                    </form>
+                    <div className="text-center">
+                        <h6>Already Registered?</h6>
+                        <Link to="/login" className="btn btn-primary mb-3 w-50">Login</Link>
                     </div>
 
-                    {
-                        status &&
-                        <div className="">
-                            {status}
-                        </div>
-
-                    }
-                </form>
-                <div className="text-center">
-                    <h6>Already Registered?</h6>
-                    <Link to="/login" className="btn btn-primary mb-3 w-50">Login</Link>
                 </div>
+            }
 
-            </div>
+
         </div>
 
     )
